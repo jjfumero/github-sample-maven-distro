7677cb7104acdda81e826008400a52bd41b894da
Juan Fumero <jjfumero@gmail.com>
2021-11-25 16:24:02 +0100

    Module - testing Github package distribution

---
+++ src/main/java/io/github/jjfumero/Sample.java
@@ -0,0 +1,11 @@
+package io.github.jjfumero;
+
+/**
+ * Example to test package distribution of maven jar files via GitHub
+ */
+public class Sample {
+
+    public static void TestMessage(String message) {
+        System.out.println("Testing maven packages with Github -- " + message);
+    }
+}
